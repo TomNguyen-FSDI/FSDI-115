2abe0fe0e31ea1880848962e63eb1b634f0265b6
TomN <tommie_89@yahoo.com>
2021-06-02 07:00:42 -0700

    added toggle to password for signup

--- assignment_03/static/js/signup.js
+++ assignment_03/static/js/signup.js
@@ -1,7 +1,7 @@
 // const togglePassword = document.querySelector('#togglePassword');
-// const togglePassword2 = document.querySelector('#togglePassword2')
 // const password = document.querySelector('#id_password');
 var eye_toggled = true;
+var eye_toggled2 = true;
 type = 'password';
 
 togglePassword.addEventListener('click', function (e) {
@@ -17,6 +17,21 @@ togglePassword.addEventListener('click', function (e) {
         type = 'password'
         eye_toggled=true;
     }
-    $('#id_password').attr('type', type);
+    $('#id_password1').attr('type', type);
 });
 
+togglePassword2.addEventListener('click', function (e) {
+    // toggle the type attribute
+    if(eye_toggled2){
+        $('#togglePassword2').removeClass("fa-eye-slash");
+        $('#togglePassword2').addClass("fa-eye");
+        type = 'text'
+        eye_toggled2=false;
+    } else{
+        $('#togglePassword2').removeClass("fa-eye");
+        $('#togglePassword2').addClass("fa-eye-slash");
+        type = 'password'
+        eye_toggled2=true;
+    }
+    $('#id_password2').attr('type', type);
+});
