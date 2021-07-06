c3c4247a92e5c81d2a0d3b510f9d4d64122b94de
TomN <nguyen.tom.anh@gmail.com>
2021-07-05 18:41:08 -0700

    move video background to login page only

--- static/js/video.js
+++ static/js/video.js
@@ -1,5 +1,11 @@
 const myVideoElement = $('#myVideo');
 
 function pauseVideo() {
-    myVideoElement.html("");
+    myVideoElement.html(`
+    <video autoplay muted loop>
+      <source src="{% static 'images/stars.mp4' %}" type="video/mp4">
+      Your browser does not support HTML5 video.
+    </video>
+    `);
+    
 };
