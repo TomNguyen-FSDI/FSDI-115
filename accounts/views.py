e009a005677dfc117e6266e2b19ff52038620bec
TomN <nguyen.tom.anh@gmail.com>
2021-07-17 11:20:50 -0700

    allow profile to create after signup is done

--- accounts/views.py
+++ accounts/views.py
@@ -20,6 +20,15 @@ class SignUpView(CreateView):
         email_in_db = User.objects.filter(email=form.data['email'])
         if len(email_in_db) > 0:
             return redirect_invalid_logic(self.request, form)
+        player = form.save()
+        player.refresh_from_db()
+        print(player.username)
+        print("form")
+        print(form.instance.email)
+        find_user = User.objects.get(email=form.instance.email)
+        create_user = Profile(user=find_user)
+        print(create_user)
+        create_user.save()
         return super().form_valid(form)
 
 
