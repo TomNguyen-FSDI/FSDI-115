57ed187f9900248ef7495b0eeb2f431f98012744
TomN <nguyen.tom.anh@gmail.com>
2021-07-25 15:11:43 -0700

    apply loading spinner to trending

--- pages/trending.py
+++ pages/trending.py
@@ -5,13 +5,16 @@ from .models import Post, Community
 class TrendingListView(ListView):
     model = Post
     template_name = 'trending/trending_list.html'
+    paginate_by = 2
 
     def get_queryset(self):
         """return the highest vote count within the last 7 days"""
         from datetime import datetime, timedelta
-        list_current = Post.objects.filter(date__gte=datetime.now()-timedelta(days=7)).annotate(like_count=Count('likes')).order_by('-like_count')
-        if len(list_current) < 7:
-            list_current = Post.objects.order_by('-date').annotate(like_count=Count('likes')).order_by('-like_count')
+        most_current_record = Post.objects.latest('date')
+        print(most_current_record)
+        list_current = Post.objects.filter(date__gte=most_current_record.date-timedelta(days=7)).annotate(like_count=Count('likes')).order_by('-like_count')
+        # if len(list_current) < 7:
+            # list_current = Post.objects.annotate(like_count=Count('likes')).order_by('-date','-like_count')
         return list_current
 
     def get_context_data(self, *args, **kwargs):
